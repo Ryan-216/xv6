84baa26e7b34f98b70c991634b24976f5c33fdc7
rtm <rtm@csail.mit.edu>
2020-10-07 13:11:03 -0400

    I have no idea what I am doing.

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+volatile int done;
 
 
 double
@@ -24,6 +25,20 @@ now()
  struct timeval tv;
  gettimeofday(&tv, 0);
  return tv.tv_sec + tv.tv_usec / 1000000.0;
+}
+
+static void
+print(void)
+{
+  int i;
+  struct entry *e;
+  for (i = 0; i < NBUCKET; i++) {
+    printf("%d: ", i);
+    for (e = table[i]; e != 0; e = e->next) {
+      printf("%d ", e->key);
+    }
+    printf("\n");
+  }
 }
 
 static void 
@@ -40,61 +55,49 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
-  // is the key already present?
-  struct entry *e = 0;
-  for (e = table[i]; e != 0; e = e->next) {
-    if (e->key == key)
-      break;
-  }
-  if(e){
-    // update the existing key.
-    e->value = value;
-  } else {
-    // the new is new.
-    insert(key, value, &table[i], table[i]);
-  }
-
+  insert(key, value, &table[i], table[i]);
 }
 
 static struct entry*
 get(int key)
 {
-  int i = key % NBUCKET;
-
-
   struct entry *e = 0;
-  for (e = table[i]; e != 0; e = e->next) {
+  for (e = table[key % NBUCKET]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
   return e;
 }
 
 static void *
-put_thread(void *xa)
+thread(void *xa)
 {
-  int n = (int) (long) xa; // thread number
+  long n = (long) xa;
+  int i;
   int b = NKEYS/nthread;
+  int k = 0;
+  double t1, t0;
 
-  for (int i = 0; i < b; i++) {
+  //  printf("b = %d\n", b);
+  t0 = now();
+  for (i = 0; i < b; i++) {
+    // printf("%d: put %d\n", n, b*n+i);
     put(keys[b*n + i], n);
   }
+  t1 = now();
+  printf("%ld: put time = %f\n", n, t1-t0);
 
-  return NULL;
-}
+  // Should use pthread_barrier, but MacOS doesn't support it ...
+  __sync_fetch_and_add(&done, 1);
+  while (done < nthread) ;
 
-static void *
-get_thread(void *xa)
-{
-  int n = (int) (long) xa; // thread number
-  int missing = 0;
-
-  for (int i = 0; i < NKEYS; i++) {
+  t0 = now();
+  for (i = 0; i < NKEYS; i++) {
     struct entry *e = get(keys[i]);
-    if (e == 0) missing++;
+    if (e == 0) k++;
   }
-  printf("%d: %d keys missing\n", n, missing);
+  t1 = now();
+  printf("%ld: get time = %f\n", n, t1-t0);
+  printf("%ld: %d keys missing\n", n, k);
   return NULL;
 }
 
@@ -103,48 +106,27 @@ main(int argc, char *argv[])
 {
   pthread_t *tha;
   void *value;
+  long i;
   double t1, t0;
 
-
   if (argc < 2) {
-    fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
+    fprintf(stderr, "%s: %s nthread\n", argv[0], argv[0]);
     exit(-1);
   }
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
   srandom(0);
   assert(NKEYS % nthread == 0);
-  for (int i = 0; i < NKEYS; i++) {
+  for (i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
-
-  //
-  // first the puts
-  //
   t0 = now();
-  for(int i = 0; i < nthread; i++) {
-    assert(pthread_create(&tha[i], NULL, put_thread, (void *) (long) i) == 0);
+  for(i = 0; i < nthread; i++) {
+    assert(pthread_create(&tha[i], NULL, thread, (void *) i) == 0);
   }
-  for(int i = 0; i < nthread; i++) {
+  for(i = 0; i < nthread; i++) {
     assert(pthread_join(tha[i], &value) == 0);
   }
   t1 = now();
-
-  printf("%d puts, %.3f seconds, %.0f puts/second\n",
-         NKEYS, t1 - t0, NKEYS / (t1 - t0));
-
-  //
-  // now the gets
-  //
-  t0 = now();
-  for(int i = 0; i < nthread; i++) {
-    assert(pthread_create(&tha[i], NULL, get_thread, (void *) (long) i) == 0);
-  }
-  for(int i = 0; i < nthread; i++) {
-    assert(pthread_join(tha[i], &value) == 0);
-  }
-  t1 = now();
-
-  printf("%d gets, %.3f seconds, %.0f gets/second\n",
-         NKEYS*nthread, t1 - t0, (NKEYS*nthread) / (t1 - t0));
+  printf("completion time = %f\n", t1-t0);
 }
